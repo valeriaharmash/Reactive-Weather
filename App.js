510a0c8f1c6471bd2a867c073f0fde5674151c06
Valeriia Harmash <valeriaharmash@gmail.com>
2023-08-16 13:34:17 -0400

    done

--- App.js
+++ App.js
@@ -1,21 +1,19 @@
-import React from 'react';
-// Import data and WeatherCard here
-
+import React from 'react'
+import WeatherCard from './components/WeatherCard'
+import cities from './data'
 
 function App() {
-    return (
-        <>
-            <h1 className = "title">REACTIVE WEATHER</h1>
-            <h3 className = "subtitle">Up to the minute weather news</h3>
-            <div className = "app">
-                {/* Render components here */}
-                
-
-
-            </div>
-            
-        </>
-    )
+	return (
+		<>
+			<h1 className='title'>REACTIVE WEATHER</h1>
+			<h3 className='subtitle'>Up to the minute weather news</h3>
+			<div className='app'>
+				{cities.map((city, index) => (
+					<WeatherCard key={index} city={city} />
+				))}
+			</div>
+		</>
+	)
 }
 
-export default App;
+export default App
